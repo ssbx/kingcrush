3246e0016ff4ddbac39c1d49211dbfe1754164e0
seb <seb@seb.seb>
2024-06-06 14:27:36 +0200

    state machine mess working

--- game.ml
+++ game.ml
@@ -7,25 +7,38 @@ open Ressources
 (* ================================================================== *)
 type view_state_t =
     SoonPlay   (* inputs disabled soon playing *)
+  | PlayInitAnimIn
+  | PlayInit
+  | PlayInitAnimOut
+  | PlayStartIn
+  | PlayStart
+  | PlayStartOut
   | Playing    (* inputs enabled, position receive all events -> to_level_over *)
   | LevelOver  (* inputs disabled, anim in/out Osd_level_over -> to_level_info *)
   | LevelInfo  (* inputs disabled, anim in/out Osd_level_info -> to_level_details *)
   | LevelDetails (* inputs disabled, anim in Osd_level_details -> to_details_wait *)
   | LevelDetailsWait (* wait click to move forward -> to_map *)
   | Map (* inputs disabled, anim in/out map -> to_map_wait *)
+  | MapIn
   | MapWait (*inputs enabled, wait_click to move forward -> to_play *)
+  | MapSelectAnimIn
+  | MapSelectOkAnimOut
+  | MapSelect
 
 let view_state : view_state_t ref = ref Playing
 
 let to_map_wait () =
-  Audio.music_play Audio.Calm;
-  Timer.fire_in 1000 (fun () -> Scr_fade.fade_in (fun () -> ()));
   view_state := MapWait
+
+let to_map_in () =
+  Audio.music_play Audio.Calm;
+  Timer.fire_in 1000 (fun () -> Scr_fade.fade_in (fun () -> to_map_wait ()));
+  view_state := MapIn
 
 let to_map () =
   Audio.music_stop ();
   Osd_level_details.start_anim_out (fun () -> ());
-  Scr_fade.fade_out (fun () -> to_map_wait ());
+  Scr_fade.fade_out (fun () -> to_map_in ());
   view_state := Map
 
 let to_level_details_wait () =
@@ -58,19 +71,74 @@ let to_soon_play () =
   Timer.fire_in 1000 (fun () -> to_play ());
   view_state := SoonPlay
 
+let to_map_select () =
+  view_state := MapSelect
+
+let to_map_select_anim_in () =
+  (* todo animate fade out map bg *)
+  Osd_map_select.start_anim_in (fun () -> to_map_select ());
+  view_state := MapSelectAnimIn
+
+let to_play_start_out () =
+  Osd_level_start.start_anim_out (fun () -> ());
+  to_play ()
+
+let to_play_start () =
+  view_state := PlayStart
+
+let to_play_start_in () =
+  Osd_level_start.start_anim_in (fun () -> ());
+  Scr_fade.fade_in (fun () -> to_play_start ());
+  view_state := PlayStartIn
+
+let to_play_init_anim_out () =
+  Osd_map_select.start_anim_out (fun () -> ());
+  Scr_fade.fade_out (fun () -> to_play_start_in ());
+  view_state := PlayInitAnimOut
+
+let to_play_init () =
+  view_state := PlayInit
+
+let to_play_init_anim_in () =
+  Audio.music_stop ();
+  Scr_fade.fade_in (fun () ->
+    Timer.fire_in 300 (fun () ->
+      Osd_level_start.start_anim_in (fun () -> to_play_init ())
+    )
+  );
+  view_state := PlayInitAnimIn
+
+let to_map_select_ok_anim_out () =
+  Osd_map_select.start_anim_out (fun () -> ());
+  Scr_fade.fade_out (fun () -> to_play_init_anim_in ());
+  view_state := MapSelectOkAnimOut
 
 let handle_sdl_event2 ~event = function
   | LevelOver -> ()
   | LevelInfo -> ()
   | LevelDetails -> ()
   | Map -> ()
+  | MapIn -> ()
   | SoonPlay -> ()
+  | PlayInitAnimIn -> ()
+  | PlayInitAnimOut -> ()
+  | PlayStartIn -> ()
+  | PlayStartOut -> ()
+  | MapSelectAnimIn -> ()
+  | MapSelectOkAnimOut -> ()
   | LevelDetailsWait ->
       if sdl_get_evt_typ event = `Mouse_button_down then to_map ()
   | Playing ->
       Brd_position.handle_sdl_event ~event
   | MapWait ->
-      if sdl_get_evt_typ event = `Mouse_button_down then to_soon_play ()
+      if sdl_get_evt_typ event = `Mouse_button_down then to_map_select_anim_in ()
+  | MapSelect ->
+      if sdl_get_evt_typ event = `Mouse_button_down then to_map_select_ok_anim_out ()
+  | PlayInit ->
+      if sdl_get_evt_typ event = `Mouse_button_down then to_play ()
+  | PlayStart ->
+      (*if sdl_get_evt_typ event = `Mouse_button_down then to_play_start_out ()*)
+      if sdl_get_evt_typ event = `Mouse_button_down then to_play ()
 
 (* ================================================================== *)
 (* model events ===================================================== *)
@@ -112,7 +180,7 @@ let update ~ticks =
   Brd_position.update ()
 
 let draw2 ~renderer = function
-  | Playing | LevelOver | LevelInfo | LevelDetails
+  | LevelOver | LevelInfo | LevelDetails
   | LevelDetailsWait | Map ->
     Scr_bg.draw ~renderer;
     Brd_squares.draw ~renderer;
@@ -121,10 +189,32 @@ let draw2 ~renderer = function
     Osd_level_over.draw ~renderer;
     Osd_level_info.draw ~renderer;
     Osd_level_details.draw ~renderer;
+    Osd_map_select.draw ~renderer;
     Scr_fade.draw ~renderer
-  | MapWait | SoonPlay ->
+  | Playing ->
     Scr_bg.draw ~renderer;
+    Brd_squares.draw ~renderer;
+    Brd_hints.draw ~renderer;
+    Brd_position.draw ~renderer
+  | PlayStartIn | PlayStart | PlayStartOut ->
+    Scr_bg.draw ~renderer;
+    Brd_squares.draw ~renderer;
+    Brd_position.draw ~renderer;
+    Osd_level_start.draw ~renderer;
     Scr_fade.draw ~renderer
+  | PlayInitAnimOut | PlayInit | PlayInitAnimIn ->
+    Scr_bg.draw ~renderer;
+    Brd_squares.draw ~renderer;
+    Brd_position.draw ~renderer;
+    Osd_level_start.draw ~renderer;
+    Scr_fade.draw ~renderer
+  | MapWait | MapIn | SoonPlay ->
+    Scr_map.draw ~renderer;
+    Scr_fade.draw ~renderer
+  | MapSelectAnimIn | MapSelect | MapSelectOkAnimOut ->
+    Scr_map.draw ~renderer;
+    Scr_fade.draw ~renderer;
+    Osd_map_select.draw ~renderer
 
 let draw ~renderer =
   draw2 ~renderer !view_state
@@ -139,6 +229,7 @@ let init ~renderer ~with_audio ~with_anims =
   Gm_streak_model.init ();
   Pieces.init ~renderer;
   Scr_bg.init ~renderer;
+  Scr_map.init ~renderer;
   Gm_streak_score.init ~renderer;
   Brd_squares.init ~renderer;
   Brd_hints.init ~renderer;
@@ -146,6 +237,8 @@ let init ~renderer ~with_audio ~with_anims =
   Osd_level_over.init ~renderer;
   Osd_level_info.init ~renderer;
   Osd_level_details.init ~renderer;
+  Osd_level_start.init ~renderer;
+  Osd_map_select.init ~renderer;
   Brd_position.init ~renderer;
   Gm_streak_model.listen handle_game_event;
   to_play ()
@@ -155,11 +248,14 @@ let release () =
   Osd_level_details.release ();
   Osd_level_info.release ();
   Osd_level_over.release ();
+  Osd_level_start.release ();
+  Osd_map_select.release ();
   Scr_fade.release ();
   Brd_hints.release ();
   Brd_squares.release ();
   Gm_streak_score.release ();
   Scr_bg.release ();
+  Scr_map.release ();
   Pieces.release ();
   (*Fonts.release ();*)
   Audio.release ();
---
+++ osd_level_start.ml
@@ -0,0 +1,104 @@
+open Tsdl
+open Gamekit
+open Gamekit.Utils
+open Ressources
+
+let rdr : Sdl.renderer option ref = ref None
+let bg_tex : Sdl.texture option ref = ref None
+let rect : Sdl.rect = Sdl.Rect.create ~x:0 ~y:0 ~w:0 ~h:0
+let get_rdr () = match !rdr with Some v -> v | None -> assert false
+let get_bg_tex () = match !bg_tex with Some v -> v | None -> assert false
+
+let enabled : bool ref = ref false
+
+let orig_x = ref 0
+let orig_y = ref 0
+
+let gen_text ~renderer ~text =
+  let surf = Fonts.get_surface text in
+  let w, h = Sdl.get_surface_size surf in
+  let tex = sdl_get_ok (Sdl.create_texture_from_surface renderer surf) in
+  Sdl.free_surface surf;
+  (tex, w, h)
+
+let init ~renderer =
+  let otex, over_w, over_h = gen_text ~renderer ~text:"LEVEL will start!!!" in
+
+  let rtex, retry_w, retry_h = gen_text ~renderer ~text:"Completion status ..." in
+
+  Sdl.Rect.set_w rect (Stdlib.max retry_w over_w + (10 * 2));
+  Sdl.Rect.set_h rect (retry_h + over_h + (10 * 2));
+  let bg_w = Sdl.Rect.w rect and bg_h = Sdl.Rect.h rect in
+
+  bg_tex :=
+    Some
+      (sdl_get_ok
+         (Sdl.create_texture renderer Sdl.Pixel.format_rgba8888 ~w:bg_w ~h:bg_h
+            Sdl.Texture.access_target));
+  sdl_try (Sdl.set_texture_blend_mode (get_bg_tex ()) Sdl.Blend.mode_blend);
+
+  sdl_try (Sdl.set_render_target renderer (Some (get_bg_tex ())));
+  sdl_try (Sdl.set_render_draw_color renderer 0 0 0 100);
+  sdl_try (Sdl.render_clear renderer);
+  let orect = Sdl.Rect.create ~x:10 ~y:10 ~w:over_w ~h:over_h in
+  sdl_try (Sdl.render_copy ~dst:orect renderer otex);
+  let rrect =
+    Sdl.Rect.create ~x:10 ~y:((10 * 2) + over_h) ~w:retry_w ~h:retry_h
+  in
+  sdl_try (Sdl.render_copy ~dst:rrect renderer rtex);
+
+  sdl_try (Sdl.set_render_target renderer None);
+
+  Sdl.destroy_texture otex;
+  Sdl.destroy_texture rtex;
+
+  Sdl.Rect.set_w rect (bg_w * 2);
+  Sdl.Rect.set_h rect (bg_h * 2);
+
+  let w = Game_state.Screen.logical_w
+  and h = Game_state.Screen.logical_h
+  and over_w = Sdl.Rect.w rect
+  and over_h = Sdl.Rect.h rect in
+  orig_x := ((w / 2) - (over_w / 2));
+  orig_y := ((h / 2) - over_h);
+  Sdl.Rect.set_x rect !orig_x;
+  Sdl.Rect.set_y rect !orig_y;
+
+  rdr := Some renderer
+
+
+let start_anim_out f =
+  Game_state.wait_for_events := false;
+  let anim = Anims.create
+    ~pt_start:(!orig_x)
+    ~pt_end:(-1000)
+    ~span:400
+    ~at_update:(fun v -> Sdl.Rect.set_x rect v)
+    ~at_end:(fun () -> enabled := false; f ())
+    Anims.Easing.Quadratic_in in
+  Anims.start anim !Game_state.ticks
+
+let start_anim_in f =
+  enabled := true;
+  Game_state.wait_for_events := false;
+  Sdl.Rect.set_y rect !orig_y;
+  Sdl.Rect.set_x rect !orig_x;
+  let anim = Anims.create
+    ~pt_start:(-1000)
+    ~pt_end:(!orig_y)
+    ~span:400
+    ~at_update:(fun v -> Sdl.Rect.set_y rect v)
+    ~at_end:f
+    Anims.Easing.Quadratic_in in
+  Anims.start anim !Game_state.ticks
+
+
+let draw ~renderer =
+  if !enabled then (
+    sdl_try (Sdl.render_copy ~dst:rect renderer (get_bg_tex ()))
+  )
+
+let release () =
+  if !bg_tex <> None then (
+    Sdl.destroy_texture (get_bg_tex ());
+    bg_tex := None)
