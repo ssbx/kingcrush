13bad608209bcbe1f033a5c17503b87e97cc6473
seb <seb@seb.seb>
2024-07-20 22:31:42 +0200

    refac

--- osd.ml
+++ osd.ml
@@ -1,69 +1,63 @@
 open CamlSDL2
 open Gamekit
 
+
+(* factorised init code , TODO factorise anim in/out most of it *)
+let gen_osd text1 text2 =
+  let line1 = Fonts.gen_text text1 in
+  let line1_rect = Sdl.Rect.make
+    ~x:10
+    ~y:10
+    ~w:line1.width
+    ~h:line1.height
+  and line2 = Fonts.gen_text text2 in
+  let line2_rect = Sdl.Rect.make
+    ~x:10
+    ~y:((10 * 2) + line1.height)
+    ~w:line2.width
+    ~h:line2.height in
+  let rect  = Sdl.Rect.make
+    ~x:0
+    ~y:0
+    ~w:((Stdlib.max line1.width line2.width) + (10 * 2))
+    ~h:(line2.height + line1.height + (10 * 2)) in
+
+  let texture = Texture2D.create rect.w rect.h in
+
+  Texture2D.draw_begin texture;
+  Texture2D.clear_with_color texture ~r:0 ~g:0 ~b:0 ~a:100;
+  Texture2D.copy line1 line1_rect;
+  Texture2D.copy line2 line2_rect;
+  Texture2D.destroy line1;
+  Texture2D.destroy line2;
+  Texture2D.draw_end texture;
+
+  let dstrect = Sdl.Rect.make
+    ~x:((Info.Display.logical_w / 2) - rect.w)
+    ~y:((Info.Display.logical_h / 2) - (rect.h * 2))
+    ~w:(rect.w * 2)
+    ~h:(rect.h * 2) in
+  (dstrect, texture)
+
+
 module Level_over = struct
-  let rdr : Sdl.Renderer.t option ref = ref None
-  let bg_tex : Sdl.Texture.t option ref = ref None
-  let rect : Sdl.Rect.t ref = ref (Sdl.Rect.make ~x:0 ~y:0 ~w:0 ~h:0)
+  let t2d : Texture2D.t option ref = ref None
+  let t2d_dst : Sdl.Rect.t ref = ref (Sdl.Rect.make ~x:0 ~y:0 ~w:0 ~h:0)
+  let orig_x : int ref = ref 0
+  let orig_y : int ref = ref 0
 
-  let get_rdr () =
-    match !rdr with
-    | Some v -> v
-    | None -> assert false
-  ;;
+  let init ~renderer:_ =
+    let (dst, tex) = gen_osd "LEVEL COMPLETE BRAVO!!!" "..." in
+    orig_x := dst.x;
+    orig_y := dst.y;
+    t2d_dst := dst;
+    t2d := Some tex
 
-  let get_bg_tex () =
-    match !bg_tex with
-    | Some v -> v
-    | None -> assert false
-  ;;
+  let draw ~renderer:_ =
+    Texture2D.copy (Option.get !t2d) !t2d_dst
 
-  let orig_x = ref 0
-  let orig_y = ref 0
-
-  let gen_text ~renderer ~text =
-    let surf = Fonts.get_surface text in
-    let w = Sdl.get_surface_width surf
-    and h = Sdl.get_surface_height surf in
-    let tex = Sdl.create_texture_from_surface renderer surf in
-    Sdl.free_surface surf;
-    tex, w, h
-  ;;
-
-  let init ~renderer =
-    let otex, over_w, over_h = gen_text ~renderer ~text:"LEVEL COMPLETE BRAVO!!!" in
-    let rtex, retry_w, retry_h = gen_text ~renderer ~text:"..." in
-    rect
-    := { !rect with
-         w = Stdlib.max retry_w over_w + (10 * 2)
-       ; h = retry_h + over_h + (10 * 2)
-       };
-    let bg_w = !rect.w
-    and bg_h = !rect.h in
-    let t = Texture2D.create ~width:bg_w ~height:bg_h () in
-    (*Texture2D.clear t {r=0;g=0;b=0;a=255};*)
-    bg_tex := Some t;
-    Sdl.set_texture_blend_mode (get_bg_tex ()) Sdl.BlendMode.BLEND;
-    Sdl.set_render_target renderer (Some (get_bg_tex ()));
-    Sdl.set_render_draw_color renderer ~r:0 ~g:0 ~b:0 ~a:100;
-    Sdl.render_clear renderer;
-    let orect = Sdl.Rect.make ~x:10 ~y:10 ~w:over_w ~h:over_h in
-    Sdl.render_copy renderer ~texture:otex ~srcrect:None ~dstrect:(Some orect);
-    let rrect = Sdl.Rect.make ~x:10 ~y:((10 * 2) + over_h) ~w:retry_w ~h:retry_h in
-    Sdl.render_copy renderer ~texture:rtex ~srcrect:None ~dstrect:(Some rrect);
-    Sdl.set_render_target renderer None;
-    Sdl.destroy_texture otex;
-    Sdl.destroy_texture rtex;
-    rect := { !rect with w = bg_w * 2; h = bg_h * 2 };
-    let w = Info.Display.logical_w
-    and h = Info.Display.logical_h
-    and over_w = !rect.w
-    and over_h = !rect.h in
-    orig_x := (w / 2) - (over_w / 2);
-    orig_y := (h / 2) - over_h;
-    rect := { !rect with x = !orig_x; y = !orig_y };
-    rdr := Some renderer
-  ;;
+  let release () =
+    Texture2D.destroy (Option.get !t2d); t2d := None
 
   let start_anim_out f =
     Info.wait_for_events := false;
@@ -72,108 +66,45 @@ module Level_over = struct
         ~pt_start:!orig_x
         ~pt_end:(-1000)
         ~span:400
-        ~at_update:(fun v -> rect := { !rect with x = v })
+        ~at_update:(fun v -> t2d_dst := { !t2d_dst with x = v })
         ~at_end:(fun () -> f ())
         Anims.Easing.Quadratic_in
     in
     Anims.start anim
-  ;;
 
   let start_anim_in f =
     Info.wait_for_events := false;
-    rect := { !rect with x = !orig_x; y = !orig_y };
+    t2d_dst := { !t2d_dst with x = !orig_x; y = !orig_y };
     let anim =
       Anims.create
         ~pt_start:(-1000)
         ~pt_end:!orig_y
         ~span:400
-        ~at_update:(fun v -> rect := { !rect with y = v })
+        ~at_update:(fun v -> t2d_dst := { !t2d_dst with y = v })
         ~at_end:f
         Anims.Easing.Quadratic_in
     in
     Anims.start anim
-  ;;
-
-  let draw ~renderer =
-    Sdl.render_copy renderer ~texture:(get_bg_tex ()) ~srcrect:None ~dstrect:(Some !rect)
-  ;;
-
-  let release () =
-    if Option.is_some !bg_tex
-    then (
-      Sdl.destroy_texture (get_bg_tex ());
-      bg_tex := None)
-  ;;
 end
 
 module Level_info = struct
-  let rdr : Sdl.Renderer.t option ref = ref None
-  let bg_tex : Sdl.Texture.t option ref = ref None
-  let rect : Sdl.Rect.t ref = ref (Sdl.Rect.make ~x:0 ~y:0 ~w:0 ~h:0)
+  let t2d : Texture2D.t option ref = ref None
+  let t2d_dst : Sdl.Rect.t ref = ref (Sdl.Rect.make ~x:0 ~y:0 ~w:0 ~h:0)
+  let orig_x : int ref = ref 0
+  let orig_y : int ref = ref 0
 
-  let get_rdr () =
-    match !rdr with
-    | Some v -> v
-    | None -> assert false
-  ;;
+  let init ~renderer:_ =
+    let (dst, tex) = gen_osd "LEVEL INFO" "Completion status ..." in
+    orig_x := dst.x;
+    orig_y := dst.y;
+    t2d_dst := dst;
+    t2d := Some tex
 
-  let get_bg_tex () =
-    match !bg_tex with
-    | Some v -> v
-    | None -> assert false
-  ;;
+  let draw ~renderer:_ =
+    Texture2D.copy (Option.get !t2d) !t2d_dst
 
-  let orig_x = ref 0
-  let orig_y = ref 0
-
-  let gen_text ~renderer ~text =
-    let surf = Fonts.get_surface text in
-    let w = Sdl.get_surface_width surf
-    and h = Sdl.get_surface_height surf in
-    let tex = Sdl.create_texture_from_surface renderer surf in
-    Sdl.free_surface surf;
-    tex, w, h
-  ;;
-
-  let init ~renderer =
-    let otex, over_w, over_h = gen_text ~renderer ~text:"LEVEL INFO" in
-    let rtex, retry_w, retry_h = gen_text ~renderer ~text:"Completion status ..." in
-    rect
-    := { !rect with
-         w = Stdlib.max retry_w over_w + (10 * 2)
-       ; h = retry_h + over_h + (10 * 2)
-       };
-    let bg_w = !rect.w
-    and bg_h = !rect.h in
-    bg_tex
-    := Some
-         (Sdl.create_texture
-            renderer
-            ~fmt:Sdl.PixelFormat.RGBA8888
-            ~access:Sdl.TextureAccess.Target
-            ~width:bg_w
-            ~height:bg_h);
-    Sdl.set_texture_blend_mode (get_bg_tex ()) Sdl.BlendMode.BLEND;
-    Sdl.set_render_target renderer (Some (get_bg_tex ()));
-    Sdl.set_render_draw_color renderer ~r:0 ~g:0 ~b:0 ~a:100;
-    Sdl.render_clear renderer;
-    let orect = Sdl.Rect.make ~x:10 ~y:10 ~w:over_w ~h:over_h in
-    Sdl.render_copy renderer ~texture:otex ~srcrect:None ~dstrect:(Some orect);
-    let rrect = Sdl.Rect.make ~x:10 ~y:((10 * 2) + over_h) ~w:retry_w ~h:retry_h in
-    Sdl.render_copy renderer ~texture:rtex ~srcrect:None ~dstrect:(Some rrect);
-    Sdl.set_render_target renderer None;
-    Sdl.destroy_texture otex;
-    Sdl.destroy_texture rtex;
-    rect := { !rect with w = bg_w * 2; h = bg_h * 2 };
-    let w = Info.Display.logical_w
-    and h = Info.Display.logical_h
-    and over_w = !rect.w
-    and over_h = !rect.h in
-    orig_x := (w / 2) - (over_w / 2);
-    orig_y := (h / 2) - over_h;
-    rect := { !rect with x = !orig_x; y = !orig_y };
-    rdr := Some renderer
-  ;;
+  let release () =
+    Texture2D.destroy (Option.get !t2d); t2d := None
 
   let start_anim_out f =
     Info.wait_for_events := false;
@@ -182,108 +113,46 @@ module Level_info = struct
         ~pt_start:!orig_x
         ~pt_end:(-1000)
         ~span:400
-        ~at_update:(fun v -> rect := { !rect with x = v })
+        ~at_update:(fun v -> t2d_dst := { !t2d_dst with x = v })
         ~at_end:(fun () -> f ())
         Anims.Easing.Quadratic_in
     in
     Anims.start anim
-  ;;
 
   let start_anim_in f =
     Info.wait_for_events := false;
-    rect := { !rect with y = !orig_y; x = !orig_x };
+    t2d_dst := { !t2d_dst with x = !orig_x; y = !orig_y };
     let anim =
       Anims.create
         ~pt_start:(-1000)
         ~pt_end:!orig_y
         ~span:400
-        ~at_update:(fun v -> rect := { !rect with y = v })
+        ~at_update:(fun v -> t2d_dst := { !t2d_dst with y = v })
         ~at_end:f
         Anims.Easing.Quadratic_in
     in
     Anims.start anim
-  ;;
-
-  let draw ~renderer =
-    Sdl.render_copy renderer ~texture:(get_bg_tex ()) ~srcrect:None ~dstrect:(Some !rect)
-  ;;
-
-  let release () =
-    if Option.is_some !bg_tex
-    then (
-      Sdl.destroy_texture (get_bg_tex ());
-      bg_tex := None)
-  ;;
 end
+
 
 module Level_details = struct
-  let rdr : Sdl.Renderer.t option ref = ref None
-  let bg_tex : Sdl.Texture.t option ref = ref None
-  let rect : Sdl.Rect.t ref = ref (Sdl.Rect.make ~x:0 ~y:0 ~w:0 ~h:0)
+  let t2d : Texture2D.t option ref = ref None
+  let t2d_dst : Sdl.Rect.t ref = ref (Sdl.Rect.make ~x:0 ~y:0 ~w:0 ~h:0)
+  let orig_x : int ref = ref 0
+  let orig_y : int ref = ref 0
 
-  let get_rdr () =
-    match !rdr with
-    | Some v -> v
-    | None -> assert false
-  ;;
+  let init ~renderer:_ =
+    let (dst, tex) = gen_osd "SOCRE BOARD" " Click to continue ..." in
+    orig_x := dst.x;
+    orig_y := dst.y;
+    t2d_dst := dst;
+    t2d := Some tex
 
-  let get_bg_tex () =
-    match !bg_tex with
-    | Some v -> v
-    | None -> assert false
-  ;;
+  let draw ~renderer:_ =
+    Texture2D.copy (Option.get !t2d) !t2d_dst
 
-  let orig_x = ref 0
-  let orig_y = ref 0
-
-  let gen_text ~renderer ~text =
-    let surf = Fonts.get_surface text in
-    let w = Sdl.get_surface_width surf
-    and h = Sdl.get_surface_height surf in
-    let tex = Sdl.create_texture_from_surface renderer surf in
-    Sdl.free_surface surf;
-    tex, w, h
-  ;;
-
-  let init ~renderer =
-    let otex, over_w, over_h = gen_text ~renderer ~text:"SCORE BOARD" in
-    let rtex, retry_w, retry_h = gen_text ~renderer ~text:"Click to continue ..." in
-    rect
-    := { !rect with
-         w = Stdlib.max retry_w over_w + (10 * 2)
-       ; h = retry_h + over_h + (10 * 2)
-       };
-    let bg_w = !rect.w
-    and bg_h = !rect.h in
-    bg_tex
-    := Some
-         (Sdl.create_texture
-            renderer
-            ~fmt:Sdl.PixelFormat.RGBA8888
-            ~access:Sdl.TextureAccess.Target
-            ~width:bg_w
-            ~height:bg_h);
-    Sdl.set_texture_blend_mode (get_bg_tex ()) Sdl.BlendMode.BLEND;
-    Sdl.set_render_target renderer (Some (get_bg_tex ()));
-    Sdl.set_render_draw_color renderer ~r:0 ~g:0 ~b:0 ~a:100;
-    Sdl.render_clear renderer;
-    let orect = Sdl.Rect.make ~x:10 ~y:10 ~w:over_w ~h:over_h in
-    Sdl.render_copy renderer ~texture:otex ~srcrect:None ~dstrect:(Some orect);
-    let rrect = Sdl.Rect.make ~x:10 ~y:((10 * 2) + over_h) ~w:retry_w ~h:retry_h in
-    Sdl.render_copy renderer ~texture:rtex ~srcrect:None ~dstrect:(Some rrect);
-    Sdl.set_render_target renderer None;
-    Sdl.destroy_texture otex;
-    Sdl.destroy_texture rtex;
-    rect := { !rect with w = bg_w * 2; h = bg_h * 2 };
-    let w = Info.Display.logical_w
-    and h = Info.Display.logical_h
-    and over_w = !rect.w
-    and over_h = !rect.h in
-    orig_x := (w / 2) - (over_w / 2);
-    orig_y := (h / 2) - over_h;
-    rect := { !rect with x = !orig_x; y = !orig_y };
-    rdr := Some renderer
-  ;;
+  let release () =
+    Texture2D.destroy (Option.get !t2d); t2d := None
 
   let start_anim_out f =
     Info.wait_for_events := false;
@@ -292,109 +161,46 @@ module Level_details = struct
         ~pt_start:!orig_x
         ~pt_end:(-1000)
         ~span:400
-        ~at_update:(fun v -> rect := { !rect with x = v })
+        ~at_update:(fun v -> t2d_dst := { !t2d_dst with x = v })
         ~at_end:(fun () -> f ())
         Anims.Easing.Quadratic_in
     in
     Anims.start anim
-  ;;
 
   let start_anim_in f =
     Info.wait_for_events := false;
-    rect := { !rect with x = !orig_x; y = !orig_y };
+    t2d_dst := { !t2d_dst with x = !orig_x; y = !orig_y };
     let anim =
       Anims.create
         ~pt_start:(-1000)
         ~pt_end:!orig_y
         ~span:400
-        ~at_update:(fun v -> rect := { !rect with y = v })
+        ~at_update:(fun v -> t2d_dst := { !t2d_dst with y = v })
         ~at_end:f
         Anims.Easing.Quadratic_in
     in
     Anims.start anim
-  ;;
-
-  let draw ~renderer =
-    Sdl.render_copy renderer ~texture:(get_bg_tex ()) ~srcrect:None ~dstrect:(Some !rect)
-  ;;
-
-  let release () =
-    if Option.is_some !bg_tex
-    then (
-      Sdl.destroy_texture (get_bg_tex ());
-      bg_tex := None)
-  ;;
 end
 
+
 module Level_confirm = struct
-  let rdr : Sdl.Renderer.t option ref = ref None
-  let bg_tex : Sdl.Texture.t option ref = ref None
-  let rect : Sdl.Rect.t ref = ref (Sdl.Rect.make ~x:0 ~y:0 ~w:0 ~h:0)
+  let t2d : Texture2D.t option ref = ref None
+  let t2d_dst : Sdl.Rect.t ref = ref (Sdl.Rect.make ~x:0 ~y:0 ~w:0 ~h:0)
+  let orig_x : int ref = ref 0
+  let orig_y : int ref = ref 0
 
-  let get_rdr () =
-    match !rdr with
-    | Some v -> v
-    | None -> assert false
-  ;;
+  let init ~renderer:_ =
+    let (dst, tex) = gen_osd "Quit?" " Click to quit ..." in
+    orig_x := dst.x;
+    orig_y := dst.y;
+    t2d_dst := dst;
+    t2d := Some tex
 
-  let get_bg_tex () =
-    match !bg_tex with
-    | Some v -> v
-    | None -> assert false
-  ;;
+  let draw ~renderer:_ =
+    Texture2D.copy (Option.get !t2d) !t2d_dst
 
-  let orig_x = ref 0
-  let orig_y = ref 0
-  let enabled : bool ref = ref false
-
-  let gen_text ~renderer ~text =
-    let surf = Fonts.get_surface text in
-    let w = Sdl.get_surface_width surf in
-    let h = Sdl.get_surface_height surf in
-    let tex = Sdl.create_texture_from_surface renderer surf in
-    Sdl.free_surface surf;
-    tex, w, h
-  ;;
-
-  let init ~renderer =
-    let otex, over_w, over_h = gen_text ~renderer ~text:"Ready to play this: select" in
-    let rtex, retry_w, retry_h = gen_text ~renderer ~text:"..." in
-    rect
-    := { !rect with
-         w = Stdlib.max retry_w over_w + (10 * 2)
-       ; h = retry_h + over_h + (10 * 2)
-       };
-    let bg_w = !rect.w
-    and bg_h = !rect.h in
-    bg_tex
-    := Some
-         (Sdl.create_texture
-            renderer
-            ~fmt:Sdl.PixelFormat.RGBA8888
-            ~access:Sdl.TextureAccess.Target
-            ~width:bg_w
-            ~height:bg_h);
-    Sdl.set_texture_blend_mode (get_bg_tex ()) Sdl.BlendMode.BLEND;
-    Sdl.set_render_target renderer (Some (get_bg_tex ()));
-    Sdl.set_render_draw_color renderer ~r:0 ~g:0 ~b:0 ~a:100;
-    Sdl.render_clear renderer;
-    let orect = Sdl.Rect.make ~x:10 ~y:10 ~w:over_w ~h:over_h in
-    Sdl.render_copy renderer ~texture:otex ~srcrect:None ~dstrect:(Some orect);
-    let rrect = Sdl.Rect.make ~x:10 ~y:((10 * 2) + over_h) ~w:retry_w ~h:retry_h in
-    Sdl.render_copy renderer ~texture:rtex ~srcrect:None ~dstrect:(Some rrect);
-    Sdl.set_render_target renderer None;
-    Sdl.destroy_texture otex;
-    Sdl.destroy_texture rtex;
-    rect := { !rect with w = bg_w * 2; h = bg_h * 2 };
-    let w = Info.Display.logical_w
-    and h = Info.Display.logical_h
-    and over_w = !rect.w
-    and over_h = !rect.h in
-    orig_x := (w / 2) - (over_w / 2);
-    orig_y := (h / 2) - over_h;
-    rect := { !rect with x = !orig_x; y = !orig_y };
-    rdr := Some renderer
-  ;;
+  let release () =
+    Texture2D.destroy (Option.get !t2d); t2d := None
 
   let start_anim_out f =
     Info.wait_for_events := false;
@@ -403,45 +209,24 @@ module Level_confirm = struct
         ~pt_start:!orig_x
         ~pt_end:(-1000)
         ~span:400
-        ~at_update:(fun v -> rect := { !rect with x = v })
-        ~at_end:(fun () ->
-          enabled := false;
-          f ())
+        ~at_update:(fun v -> t2d_dst := { !t2d_dst with x = v })
+        ~at_end:(fun () -> f ())
         Anims.Easing.Quadratic_in
     in
     Anims.start anim
-  ;;
 
   let start_anim_in f =
-    enabled := true;
     Info.wait_for_events := false;
-    rect := { !rect with x = !orig_x; y = !orig_y };
+    t2d_dst := { !t2d_dst with x = !orig_x; y = !orig_y };
     let anim =
       Anims.create
         ~pt_start:(-1000)
         ~pt_end:!orig_y
         ~span:400
-        ~at_update:(fun v -> rect := { !rect with y = v })
+        ~at_update:(fun v -> t2d_dst := { !t2d_dst with y = v })
         ~at_end:f
         Anims.Easing.Quadratic_in
     in
     Anims.start anim
-  ;;
-
-  let draw ~renderer =
-    if !enabled
-    then
-      Sdl.render_copy
-        renderer
-        ~texture:(get_bg_tex ())
-        ~srcrect:None
-        ~dstrect:(Some !rect)
-  ;;
-
-  let release () =
-    if !bg_tex <> None
-    then (
-      Sdl.destroy_texture (get_bg_tex ());
-      bg_tex := None)
-  ;;
 end
+
